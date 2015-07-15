7e7d12502d9dd7e2713a6460f28284e9c238cb31
Larry.Hwang <larryhwang@foxmail.com>
2015-07-15 23:39:50 +0800

    Initial Commit

---
+++ JsonTest1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JsonTest1
+//
+//  Created by Larry.Hwang on 15/7/15.
+//  Copyright (c) 2015年 Larry.Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
